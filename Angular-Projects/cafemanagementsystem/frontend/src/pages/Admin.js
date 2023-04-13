4cae763c743398c93ccdf3530671f56b5394590b
Patrick <papybp@gmail.com>
2023-04-12 23:32:53 -0500

    zczxczxc

--- Angular-Projects/cafemanagementsystem/frontend/src/pages/Admin.js
+++ Angular-Projects/cafemanagementsystem/frontend/src/pages/Admin.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Admin.css'
 export default function Admin() {
   return (
     <div className='container-fluid d-flex bg-light' style={{height:"100vh"}}>
@@ -18,8 +18,10 @@ export default function Admin() {
                <h6>Manage Category</h6> 
                 <button className='btn btn-primary'><i class="bi bi-plus-circle-fill"></i> Add Category</button>
             </div>
-            <div className='filterContainer mt-4 shadow'>
-                <input text="text" className='filterInput' />
+            <div className='field mt-4 shadow'>
+                
+                <input id='input' text="text"  />
+                <label  htmlFor='input'>Filter</label>
             </div>
             <div className='tableContainer shadow'></div>
         </div>
